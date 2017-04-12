528e2d363153c53690175bf00a1c1f44e12e5c2d
oceane <oceane.gatien.cef@hotmail.fr>
2017-04-12 17:27:02 +0200

    add css bootstrap

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ $( document ).ready( function() {
 	
 
 	$.ajax({
-		url: "http://api.openweathermap.org/data/2.5/weather?id=6448311&appid=5a932bb55109527ecdb2d9efa5eb07fd&mode=html&lang={fr}"
+		url: "http://api.openweathermap.org/data/2.5/weather?id=6448311&appid=5a932bb55109527ecdb2d9efa5eb07fd&mode=html&lang=fr"
 	})
 	.done(function (data) {
-		$('#affich').append(data);
+		$('#affichJour').append(data);
 	})
 	.fail(function () {
 		console.log("error");
-		$('#a').append('<img class="img-responsive" src="http://68.media.tumblr.com/91748c0d0b4489e05a4864e0c583c473/tumblr_mzqakbdhnx1su5t9oo1_400.gif" alt="Loading">')
+		$('#affichJour').append('<img class="img-responsive col-md-2 col-md-offset-5" src="http://68.media.tumblr.com/91748c0d0b4489e05a4864e0c583c473/tumblr_mzqakbdhnx1su5t9oo1_400.gif" alt="Loading">').css('font-weight: 200px;')
 	})
 	.always(function () {
 		console.log("Complete");
